d7f280ff3ffff6c4e83491e05078590c944dae33
Benas <bbrazdziunas@gavea.co.uk>
2021-05-08 19:57:02 +0100

    added sd card

--- firmware/config/boards/core8/compile_core8.bat
+++ firmware/config/boards/core8/compile_core8.bat
@@ -2,7 +2,7 @@
 cd ../../../
 make -j12 PROJECT_BOARD=core8 PROJECT_CPU=ARCH_STM32F4
 cd config/boards/core8/
-@REM bash.exe gen_config_board.sh core8 core8
+bash.exe gen_config_board.sh core8 core8
 
 
 
